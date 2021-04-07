6a02dd3b7c5ebeeea2a3029971a54c764c841c27
Petr Tolmachev <tolmachev.pk@phystech.edu>
2021-04-07 15:12:38 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/wordParserFinder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wordParserFinder.dir/main.cpp.o"
+  "wordParserFinder"
+  "wordParserFinder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wordParserFinder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
